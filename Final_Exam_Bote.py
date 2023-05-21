996e6d676a6eb9b73070ff185d7b2052d995bb7d
briantjerome <112708014+briantjerome@users.noreply.github.com>
2023-05-21 10:47:03 +0800

    Update Final_Exam_Bote.py

--- Final_Exam_Bote.py
+++ Final_Exam_Bote.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import tensorflow as tf
-from tensorflow.keras.datasets import fashion_mnist
+from tensorflow.keras.preprocessing import image
+import numpy as np
 
 @st.cache(allow_output_mutation=True)
 def load_model():
@@ -18,16 +19,17 @@ file = st.file_uploader("Choose a fashion accessory image (bag, shirt, etc.) fro
 if file is None:
     st.text("Please upload an image file")
 else:
-    image = tf.image.decode_image(file.read(), channels=1)
-    image = tf.image.resize(image, [28, 28]) / 255.0
-    image = tf.expand_dims(image, axis=0)
+    img = image.load_img(file, target_size=(28, 28), grayscale=True)
+    img_array = image.img_to_array(img)
+    img_array /= 255.0
+    img_array = np.expand_dims(img_array, axis=0)
 
-    prediction = model.predict(image)
+    prediction = model.predict(img_array)
     class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress',
                    'Coat', 'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
-    
-    predicted_class_index = tf.argmax(prediction[0]).numpy()
+
+    predicted_class_index = np.argmax(prediction[0])
     predicted_class = class_names[predicted_class_index]
 
-    st.image(image[0], use_column_width=True)
+    st.image(img, use_column_width=True)
     st.success("Predicted Class: " + predicted_class)
