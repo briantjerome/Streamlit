252d07b3ded3ae5859049c5d078c588caa12aa7b
briantjerome <112708014+briantjerome@users.noreply.github.com>
2023-05-21 11:00:58 +0800

    Update Final_Exam_Bote.py

--- Final_Exam_Bote.py
+++ Final_Exam_Bote.py
@@ -19,14 +19,15 @@ file = st.file_uploader("Choose a fashion accessory image (bag, shirt, etc.) fro
 if file is None:
     st.text("Please upload an image file")
 else:
-    img = image.load_img(file, target_size=(128, 128), grayscale=True)
+    img = image.load_img(file, target_size=(128, 128), color_mode="grayscale")
     img_array = image.img_to_array(img)
     img_array = np.expand_dims(img_array, axis=0)
     img_array /= 255.0
 
-    predicted_class = model.predict_classes(img_array)[0]
+    prediction = model.predict(img_array)
+    predicted_class_index = np.argmax(prediction[0])
     class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress',
                    'Coat', 'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
 
     st.image(img, use_column_width=True)
-    st.success("Predicted Class: " + class_names[predicted_class])
+    st.success("Predicted Class: " + class_names[predicted_class_index])
