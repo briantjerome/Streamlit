75656a36a5c56c5cb5b45bc8369b9b508281d2bd
briantjerome <112708014+briantjerome@users.noreply.github.com>
2023-05-21 10:44:26 +0800

    Update Final_Exam_Bote.py

--- Final_Exam_Bote.py
+++ Final_Exam_Bote.py
@@ -1,32 +1,33 @@
 import streamlit as st
 import tensorflow as tf
+from tensorflow.keras.datasets import fashion_mnist
 
 @st.cache(allow_output_mutation=True)
 def load_model():
-  model=tf.keras.models.load_model('best_model.h5')
-  return model
-model=load_model()
+    model = tf.keras.models.load_model('best_model.h5')
+    return model
+
+model = load_model()
+
 st.write("""
-# Fashion_Mnist accessory system"""
-)
-file=st.file_uploader("Choose any fashion accessories eg bag, shirt from computer",type=["jpg","png"])
-import cv2
-from PIL import Image,ImageOps
-import numpy as np
-def import_and_predict(image_data,model):
-    size=(64,64)
-    image=ImageOps.fit(image_data,size,Image.ANTIALIAS)
-    img=np.asarray(image)
-    img_reshape=img[np.newaxis,...]
-    prediction=model.predict(img_reshape)
-    return prediction
+# Fashion MNIST Accessory System
+""")
+
+file = st.file_uploader("Choose a fashion accessory image (bag, shirt, etc.) from your computer", type=["jpg", "png"])
+
 if file is None:
     st.text("Please upload an image file")
 else:
-    image=Image.open(file)
-    st.image(image,use_column_width=True)
-    prediction=import_and_predict(image,model)
-    class_names=['T-shirt/top', 'Trouser', 'Pullover', 'Dress',
-                'Coat', 'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
-    string="OUTPUT : "+class_names[np.argmax(prediction)]
-    st.success(string)
+    image = tf.image.decode_image(file.read(), channels=1)
+    image = tf.image.resize(image, [28, 28]) / 255.0
+    image = tf.expand_dims(image, axis=0)
+
+    prediction = model.predict(image)
+    class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress',
+                   'Coat', 'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+    
+    predicted_class_index = tf.argmax(prediction[0]).numpy()
+    predicted_class = class_names[predicted_class_index]
+
+    st.image(image[0], use_column_width=True)
+    st.success("Predicted Class: " + predicted_class)
